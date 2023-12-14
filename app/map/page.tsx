d50003bd9d14ebcf13cde88f7451271512f6b9ce
MagicSakuraD <ckgoforit@outlook.com>
2023-12-14 15:15:35 +0800

    try use storybook

---
+++ app/map/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React from "react";
+import {
+  ImageOverlay,
+  MapContainer,
+  Marker,
+  Polyline,
+  Popup,
+  TileLayer,
+  useMap,
+  ScaleControl,
+  useMapEvent,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const MapPage = () => {
+  const position: [number, number] = [31.005, 125.021];
+  return (
+    <main className="container mx-auto">
+      {/* <div className="w-96 h-96 m-5 bg-current opacity-30"></div> */}
+      <div className="card bg-base-100 shadow-xl glass">
+        {" "}
+        <div>
+          <MapContainer
+            center={position}
+            zoom={13}
+            scrollWheelZoom={true}
+            className="h-[35rem] rounded-lg"
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                纬度: {position[0]}, 经度: {position[1]}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default MapPage;
